d9ed46646fb2b21dd8b69d39bb85897b1f7b5d27
Mirza Hafiz <mirzahm14@gmail.com>
2023-11-20 18:26:53 +0700

    add start func

--- werewolf_bot.py
+++ werewolf_bot.py
@@ -1,21 +1,23 @@
 from discord.ext import commands
 import discord
 import asyncio
+import time
+import random
 
 BOT_TOKEN = open('token.txt', 'r').read().strip()
 CHANNEL_ID = 1174972462367260675
 
 class Player:
-    ids = {}
     def __init__(self, user, role=None, emoji=None):
-        ids[len(ids)] = self
         self.user = user
         self.role = role
         self.emoji = emoji
 
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 users = []
+players=[]
 state = 'not playing'
+roles = ['werewolf', 'seer', 'bodyguard', 'villager', 'lycan']
 
 @bot.event
 async def on_ready():
@@ -72,4 +74,52 @@ async def play(ctx):
 
 @bot.command()
 async def start(ctx):
+    global players, users, isGameEnded, emoji, killed, roles
+    for user in users:
+        player = Player(user, await draw(user), users.index(user))
+        players.append(player)
+
+    await ctx.send('Permainan dimulai')
+    #malam 1
+    await ctx.send('Malam telah tiba')
+    time.sleep(3)
+    await ctx.send('Seer silahkan bangun')
+    await seerTurn(ctx)
+    time.sleep(5)
+
+    #pagi 1
+    await ctx.send('Pagi telah tiba, silakan kalian berdiskusi selama 2 menit')
+    time.sleep(60)
+    await ctx.send('Waktu diskusi tersisa 1 menit')
+    time.sleep(50)
+    await ctx.send('Waktu diskusi tersisa 10 detik')
+    time.sleep(10)
+    await ctx.send('Waktu habis silakan melakukan voting')
+
+    #vote func
+
+    while isGameEnded == False:
+        await ctx.send("Malam telah tiba, Silakan tutup mata kalian")
+        time.sleep(5)
+        await wolfTurn(ctx)
+        await seerTurn(ctx)
+        await guardTurn(ctx)
+
+
+async def draw(user):
+    global roles
+    
+    role = random.choice(roles)
+    roles.remove(role)
+
+    print(f"assigned {user} {roles}")
+
+    if role != None:
+        await send_msg(user, f"Role kamu adalah {role}")
+    return role
+
+async def send_msg(user, msg):
+    dm = await user.create_dm()
+    await dm.send(msg)
+
 bot.run(BOT_TOKEN)
